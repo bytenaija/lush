765c7dbef76f7666a6002ccd9f0aac411081a478
Everistus Olumese <everistusolumese@gmail.com>
2018-07-03 14:23:45 +0100

    Heroku Deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/about", (req, res, next) => {
 })
 
 
-let port = process.env.port || 3000;
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
